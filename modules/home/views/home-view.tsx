1daab0306dcf95fe055bdaed7926990a959c8ea5
Euger Bonete <azkriven17@gmail.com>
2025-08-13 12:33:24 +0800

    trending-anime-section

--- modules/home/views/home-view.tsx
+++ modules/home/views/home-view.tsx
@@ -3,18 +3,27 @@
 import { useTRPC } from "@/trpc/client";
 import { useQuery } from "@tanstack/react-query";
 import { CarouselSection } from "../ui/carousel-section";
+import { TrendingSection } from "../ui/trending-section";
 
 export function HomeView() {
   const trpc = useTRPC();
-  const { data, isLoading, isError } = useQuery(trpc.home.queryOptions());
-  console.log(data);
+  const { data, isLoading, isError, refetch } = useQuery(
+    trpc.home.queryOptions()
+  );
   return (
-    <div>
+    <main>
       <CarouselSection
         data={data?.data.spotlightAnimes || []}
         isLoading={isLoading}
         isError={isError}
+        refetch={refetch}
       />
-    </div>
+      <TrendingSection
+        data={data?.data.trendingAnimes || []}
+        isLoading={isLoading}
+        isError={isError}
+        refetch={refetch}
+      />
+    </main>
   );
 }
